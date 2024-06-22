b07f654b7a3c997110602cd1bfc154a3b8f7e9e5
Aliyan Arif <143800651+RAGUKHAN009@users.noreply.github.com>
2024-06-23 02:24:58 +0500

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,6 +8,45 @@ let isDragging = false;
 let rotation = 0;
 let prevRotation = 0;
 
+// Add touch event listeners
+circle.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+  startY = e.touches[0].clientY;
+  isDragging = true;
+});
+
+circle.addEventListener('touchmove', (e) => {
+  if (isDragging) {
+    endX = e.touches[0].clientX;
+    endY = e.touches[0].clientY;
+    const angle = Math.atan2(endY - startY, endX - startX);
+    const distance = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
+    if (distance > 20) { // adjust the distance threshold to your needs
+      isDragging = false;
+    }
+    rotation = (angle * 180) / Math.PI;
+    circle.style.transform = `rotate(${rotation}deg)`;
+    if (rotation > prevRotation) {
+      // Clockwise rotation
+      hiddenButton.value = 'button1';
+    } else if (rotation < prevRotation) {
+      // Anticlockwise rotation
+      hiddenButton.value = 'button2';
+    }
+    prevRotation = rotation;
+  }
+});
+
+circle.addEventListener('touchend', () => {
+  if (hiddenButton.value === 'button1') {
+    button1.click(); // simulate a click on button 1
+  } else if (hiddenButton.value === 'button2') {
+    button2.click(); // simulate a click on button 2
+  }
+  isDragging = false;
+});
+
+// Add mouse event listeners for desktop
 circle.addEventListener('mousedown', (e) => {
   startX = e.clientX;
   startY = e.clientY;
@@ -45,10 +84,10 @@ circle.addEventListener('mouseup', () => {
   isDragging = false;
 });
 
-// button1.addEventListener('click', () => {
-//   alert('Button 1 pressed!');
-// });
+button1.addEventListener('click', () => {
+  alert('Button 1 pressed!');
+});
 
-// button2.addEventListener('click', () => {
-//   alert('Button 2 pressed!');
-// });
+button2.addEventListener('click', () => {
+  alert('Button 2 pressed!');
+});
